46d2300d6207eccd7ebd30b36d1b36434178da81
Isis Maximo Mello <isismaximomello@gmail.com>
2026-06-25 14:57:11 -0300

    Mudança tipo dos objetos nas funções para Bplus ser genérica

--- Bplus.h
+++ Bplus.h
@@ -1,13 +1,14 @@
 #define ORDEM 4
-#define TAMANHO_Pagina 4096
 #include <stdbool.h>
-#include "RH.h"
 
+typedef struct {
+    char valor[100];
+} Chave;
 
 typedef struct Pagina {
-    funcionario chave[ORDEM + 1];
+    int Chave[ORDEM + 1];
     int filho[ORDEM + 2];
-    int pai; //pai do vetor de chave
+    int pai; //pai do vetor de Chave
     int indice; //endereço da página
     int proximaFolha; //endereço irma adjacente
     int qtElementos; //qt elementos ocupados 
@@ -19,7 +20,7 @@ typedef struct {
     long raiz_rid;          // Onde a raiz da árvore está no arquivo (RID)
     long topo_lista_livre;  // Ponteiro para reuso de nós deletados (isso o GPT que fez, funciona como uma lista emcadeada
                             // é uma lista encadeada de nós livres, cada nó livre tem um ponteiro para o próximo nó livre
-    int tamanho_chave;      // Configurado na inicialização (ex: sizeof(ChaveComposta))
+    int tamanho_Chave;      // Configurado na inicialização (ex: sizeof(Chave))
     int tamanho_dado;       // Configurado na inicialização (ex: sizeof(Funcionario))
 } CabecalhoArquivo;
 
@@ -27,8 +28,8 @@ typedef struct {
 Pagina *criaPagina(int ordem);
 void inicializarPagina(Pagina *Pagina, int ordem, int index, int tipo);
 void destroiPagina(Pagina *p);
-void inserirElemento(Pagina *p, int chave);
-void removerElemento(Pagina *p, int chave);
+void inserirElemento(Pagina *p, Chave chave);
+void removerElemento(Pagina *p, Chave chave);
 void verificarOverflow(Pagina *p);
 void verificarUnderflow(Pagina *p);
 void ordenarPaginaFolha(Pagina *p);
@@ -36,8 +37,7 @@ void ordenarPaginaFolha(Pagina *p);
 // funções para a árvore
 void inicializarArvore();
 void imprimirArvore();
-int buscarChave(chaveComposta chave, int *indexPagina);
-void inserirChave(chaveComposta chave, int enderecoRegistro);
-void deletarChave(chaveComposta chave);
-int removerChave(chaveComposta chave);
-void imprimirChavesIntervalo(chaveComposta inicial, chaveComposta final);
+int buscarChave(Chave chave, int *indexPagina);
+void inserirChave(Chave chave, int enderecoRegistro);
+void deletarChave(Chave chave);
+void imprimirChavesIntervalo(Chave inicial, Chave final);
